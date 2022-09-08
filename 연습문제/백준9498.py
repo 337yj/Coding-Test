480e7a4da52ceec7383e15347a444d154b4e765d
337yj <110447844+337yj@users.noreply.github.com>
2022-09-08 20:50:58 +0900

    Create 백준9498.py

---
+++ 연습문제/백준9498.py
@@ -0,0 +1,35 @@
+score = int(input())
+
+if score >= 90 :
+    print('A')
+elif score >= 80 :
+    print('B')
+elif score >= 70 :
+    print('C')
+elif score >= 60 :
+    print('D')
+else:
+    print('F')
+    
+
+score = int(input())
+
+if 100>= score >= 90 :
+    print('A')
+elif 90 > score >= 80 :
+    print('B')
+elif 80 > score >= 70 :
+    print('C')
+elif 70 > score >= 60 :
+    print('D')
+else:
+    print('F')
+    
+    
+# 한줄코드
+
+if a >= 90: print("A")
+elif a >= 80: print("B")
+elif a >= 70: print("C")
+elif a >= 60: print("D")
+else: print("F")
