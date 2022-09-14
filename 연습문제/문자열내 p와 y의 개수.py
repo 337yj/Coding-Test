df1ed2bcfef7b693f1dfe2b02e633abd25454805
337yj <110447844+337yj@users.noreply.github.com>
2022-09-14 22:11:25 +0900

    Create 문자열내 p와 y의 개수.py

---
+++ 연습문제/문자열내 p와 y의 개수.py
@@ -0,0 +1,11 @@
+def solution(s):
+
+    return s.lower().count('p') == s.lower().count('y')
+  
+  
+#--------------------------------------------------------------
+# JS
+
+# function solution(s){
+#     return s.toUpperCase().split("P").length === s.toUpperCase().split("Y").length;
+# }
