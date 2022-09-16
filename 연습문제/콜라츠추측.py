3f1eafbd2abee4370298def41445c8089d5e4d81
337yj <110447844+337yj@users.noreply.github.com>
2022-09-17 02:00:07 +0900

    Create 콜라츠추측.py

---
+++ 연습문제/콜라츠추측.py
@@ -0,0 +1,9 @@
+def solution(num):
+    if num == 1: return 0
+    for i in range(500):
+        num = num / 2 if num % 2 == 0 else (num*3) + 1
+        if num == 1:
+            return i + 1
+    return -1
+
+print(solution(1))
