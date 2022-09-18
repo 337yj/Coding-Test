3e9325f41dddbb002ed38f5efa9187a5623cb870
337yj <110447844+337yj@users.noreply.github.com>
2022-09-18 19:21:50 +0900

    Create 내적.py

---
+++ 연습문제/내적.py
@@ -0,0 +1,16 @@
+def solution(a, b):
+  answer = 0
+  for i in range(len(a)):
+    answer += a[i]*b[i]
+    
+   return answer
+
+
+#------------------------------------JS
+# function solution(a, b) {
+#     var answer = 0;
+#     for(let i = 0; i < a.length; i++){
+#         answer += a[i]*b[i];
+#     }
+#     return answer;
+# }
