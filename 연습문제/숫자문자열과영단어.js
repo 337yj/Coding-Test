69ce262187dfb97f9eb331ea44144e41303177bc
337yj <110447844+337yj@users.noreply.github.com>
2022-09-28 23:33:15 +0900

    Create 숫자문자열과영단어.js

---
+++ 연습문제/숫자문자열과영단어.js
@@ -0,0 +1,21 @@
+function solution(s) {
+    let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    var answer = s;
+
+    for(let i = 0; i < numbers.length; i++) {
+        let arr = answer.split(numbers[i]);
+        answer = arr.join(i);
+    }
+
+    return Number(answer);
+}
+
+
+------------------------------------------------------py
+
+
+def solution(s):
+    dic = {'zero':'0', 'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
+    for key in dic:
+        s = s.replace(key, dic[key])
+    return int(s)
