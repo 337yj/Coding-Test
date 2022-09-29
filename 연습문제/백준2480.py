ff883730d5c2e8c80e82755081f1cd201194e4c2
337yj <110447844+337yj@users.noreply.github.com>
2022-09-29 23:41:38 +0900

    Update 백준2480.py

--- 연습문제/백준2480.py
+++ 연습문제/백준2480.py
@@ -10,3 +10,30 @@ elif b == c:
     print(1000+b*100)
 else:
     print(100 * max(a,b,c))
+
+    
+--------------------------------------------js
+
+const input = require("fs")
+    .readFileSync("/dev/stdin")
+    .toString()
+    .split(" ")
+    .map((value) => +value);
+
+input.sort(function (a, b) {
+    return b - a;
+});
+
+const [A, B, C] = input;
+
+if (A !== B && A !== C && B !== C) {
+    console.log(A * 100);
+} else if (A === B && B !== C) {
+    console.log(1000 + A * 100);
+} else if (B === C && C !== A) {
+    console.log(1000 + C * 100);
+} else if (C === A && A !== B) {
+    console.log(1000 + C * 100);
+} else {
+    console.log(10000 + A * 1000);
+}
