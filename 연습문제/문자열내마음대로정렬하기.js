ad9df0cca7540b03c8b892972150828f33cd5527
337yj <110447844+337yj@users.noreply.github.com>
2022-10-02 16:36:53 +0900

    Create 문자열내마음대로정렬하기.js

---
+++ 연습문제/문자열내마음대로정렬하기.js
@@ -0,0 +1,12 @@
+function solution(strings, n) {
+    strings.sort((a,b)=>{
+        if(a[n] > b[n]) return 1;
+        if(b[n] > a[n]) return -1;
+
+        if(a > b) return 1;
+        if(b > a) return -1;
+
+        return 0;
+    });
+    return strings;
+}
