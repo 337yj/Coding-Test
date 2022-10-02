c500ca86b2ff24dc52bd4c79634c9fd7386c43dc
337yj <110447844+337yj@users.noreply.github.com>
2022-10-02 16:39:44 +0900

    Update 문자열내마음대로정렬하기.js

--- 연습문제/문자열내마음대로정렬하기.js
+++ 연습문제/문자열내마음대로정렬하기.js
@@ -17,3 +17,7 @@ function solution(strings, n) {
 def solution(strings, n):
     strings.sort() 
     return sorted(strings, key=lambda x:x[n])
+
+    
+def solution(strings, n):
+    return sorted(strings, key=lambda x:(x[n],x))
