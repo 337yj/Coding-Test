ee3b6b876e23a703d93e687c5c1cd7a82a8fd993
337yj <110447844+337yj@users.noreply.github.com>
2022-10-14 21:31:16 +0900

    Update 입실퇴실.js

--- 연습문제/입실퇴실.js
+++ 연습문제/입실퇴실.js
@@ -27,3 +27,22 @@ function solution(enter, leave) {
     
     return answer;
 }
+
+
+-----------------------------------------------------------py
+
+def solution(enter, leave):
+    answer = [0] * len(enter)
+
+    room = []
+    e_idx = 0
+    for l in leave:
+        while l not in room:
+            room.append(enter[e_idx])
+            e_idx += 1
+        room.remove(l)
+        for p in room:
+            answer[p - 1] += 1
+        answer[l - 1] += len(room)
+
+    return answer
