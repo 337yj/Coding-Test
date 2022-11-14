a1d60ce19b83bcd0ec841abe5dc77f2b7d1892c5
Yun Jeong <110447844+337yj@users.noreply.github.com>
2022-11-14 18:50:49 +0900

    Create 카펫.js

---
+++ 연습문제/카펫.js
@@ -0,0 +1,11 @@
+function solution(brown, red) {
+    var answer = [];
+    for (var i = 3; i <= (brown+red)/i; i++) {
+        var x = Math.floor((brown+red)/i);
+        if( (x-2)*(i-2)=== red) {
+            break;
+        }
+    }
+ 
+    return [x,i];  // 가로 x, 세로 i
+}
