3e737d8dc9f9ae02df8151e63619537db2aa7875
Yun Jeong Lee <110447844+337yj@users.noreply.github.com>
2022-12-01 06:45:07 +0900

    Create 최댓값.js

---
+++ 연습문제/최댓값.js
@@ -0,0 +1,14 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n').map(x=> Number(x));
+
+let max = input[0];
+let maxIdx = 0;
+
+for (let i = 1; i < 9; i++) {
+  if (max < input[i]) {
+    max = input[i];
+    maxIdx = i;
+  }
+}
+
+console.log(max);
+console.log(maxIdx + 1);
