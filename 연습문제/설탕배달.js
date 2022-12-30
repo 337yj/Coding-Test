78da8971e157825a49ebf835964ef75ba2f0f185
Yun Jeong Lee <110447844+337yj@users.noreply.github.com>
2022-12-30 19:54:01 +0900

    Create 설탕배달.js

---
+++ 연습문제/설탕배달.js
@@ -0,0 +1,18 @@
+let input = require('fs').readFileSync('/dev/stdin');
+
+let count = 0;
+
+while (true) {
+  if (input % 5 === 0) {
+    console.log(input / 5 + count);
+    break;
+  }
+    
+  if (0 > input) {
+    console.log(-1);
+    break;
+  }
+
+  count++;
+  input -= 3;
+}
