e78fff53f9f587e71723e42ccf6cbd3aec1c643e
Yun Jeong Lee <110447844+337yj@users.noreply.github.com>
2023-01-11 20:16:29 +0900

    Create 소수.js

---
+++ 연습문제/소수.js
@@ -0,0 +1,25 @@
+let input = require('fs');.readFileSync('예제.txt').toString().split('\n');
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+let primeNumbers = [];
+let sum = 0;
+
+for(let target = a; target <= b; target++){
+    for(let p = 2; p <= target; p++){
+        if(p*p > target){
+            primeNumbers.push(target);
+            sum += target;
+            break;
+        }
+        if(target % p === 0){
+            break;
+        }
+    }
+}
+if(primeNumbers.length === 0){
+    console.log(-1);
+}else{
+    console.log(sum);
+    console.log(primeNumbers[0]);
+}
