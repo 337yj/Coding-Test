2e65a39c21fe07d79bf081e6c7858aa852347e62
Yun Jeong Lee <110447844+337yj@users.noreply.github.com>
2023-01-29 00:01:11 +0900

    Update ATM.js

--- 연습문제/ATM.js
+++ 연습문제/ATM.js
@@ -1,26 +1,16 @@
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-const arr = input[1].split(' ').map(Number).sort((a,b)=>a-b);
-
-let total =0;
-let sum=0;
-for(let i =0; i<arr.length; i++){
-    total +=sum +arr[i];
-    sum += arr[i];
-}
-console.log(total);
-
-// --------------------------------------------------------------------ㄴㅇㄹ----------
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const N = +input.shift();
-.map(Number).sort((a,b) => a-b);
 
-let totalr = 0;
+const lines = input[0].split(' ').map(Number).sort((a,b) => a-b);
+
+let answer = 0;
 for(let i=0; i<N; i++) {
-    let sum = arr[i];
+    let sum = lines[i];
     for(let j=0; j<i; j++) {
-        sum += arr[j];
+        sum += lines[j];
     }
-    total += sum;
+    answer += sum;
 }
-console.log(total);
+
+console.log(answer);
