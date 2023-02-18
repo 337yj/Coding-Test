eacbd5fdf9b318e02abffd3b4ec1c67267868366
Yun Jeong Lee <110447844+337yj@users.noreply.github.com>
2023-02-19 05:59:38 +0900

    Update 팩토리얼.js

--- 연습문제/팩토리얼.js
+++ 연습문제/팩토리얼.js
@@ -9,3 +9,19 @@ function solution(input){
 }
 
 console.log(solution(input))
+
+
+//----------------------------------------------------------------------------------
+function solution(n) {
+    function DFS(n){
+        if(n===1) return 1;
+        else{
+            return n * DFS(n-1)
+        }
+    }
+    let answer = DFS(n)
+    
+    return answer
+}
+
+console.log(solution(5));
