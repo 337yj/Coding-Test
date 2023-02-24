86705f40c9d23646af3177551514874362f51840
Yun Jeong Lee <110447844+337yj@users.noreply.github.com>
2023-02-24 23:53:39 +0900

    Update 송아지찾기(BFS).js

--- 연습문제/송아지찾기(BFS).js
+++ 연습문제/송아지찾기(BFS).js
@@ -26,3 +26,30 @@ console.log(solution(5, 14));
 //   4    6   10
 // 3 5 9 => 5는 이전에 해봤으니 생략해도됨 어차피 다음레벨에 +1하면 6나오는거니까
 // 14가 나올 때까지 L++(만들어서 넣을 때 확인하고 return)
+
+
+//--------------------------------------------------------------------
+
+function solution(s, e){  
+                let answer=0;
+                let ch=Array.from({length:10001}, ()=>0);
+                let queue=[];
+                queue.push(s);
+                ch[s]=1;
+                let L=0;
+                while(queue.length){
+                    let len=queue.length;
+                    for(let i=0; i<len; i++){
+                        let x=queue.shift();
+                        for(let nx of [x-1, x+1, x+5]){
+                            if(nx===e) return L+1;
+                            if(nx>0 && nx<=10000 && ch[nx]===0){
+                                ch[nx]=1;
+                                queue.push(nx);
+                            }
+                        }
+                    }
+                    L++;
+                }
+                return answer;
+            }
