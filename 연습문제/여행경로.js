0fd7e116173d6f30db1367b149615105d248fdf3
Yun Jeong Lee <110447844+337yj@users.noreply.github.com>
2023-03-19 23:48:49 +0900

    Update 여행경로.js

--- 연습문제/여행경로.js
+++ 연습문제/여행경로.js
@@ -1,31 +1,3 @@
-function solution(tickets) {
-    const answer = [];
-    const goal = tickets.length + 1;
-    const ch = Array.from({length: tickets.length}, _ => 0);
-    
-    const dfs = (path) => {
-        if (path.length === goal) {
-            answer.push(path)
-        } else {
-            for(const i in tickets) {
-                if(ch[i] === 0) {
-                    const [start, end] = tickets[i];
-                    if (path[path.length - 1] === start) {
-                        ch[i] = 1;
-                        dfs([...path, end]);
-                        ch[i] = 0;
-                    }
-                }
-            }
-        }
-    }
-    
-    dfs(["ICN"]);
-    
-    return answer.sort()[0];
-}
-
--------------------------------------------------------------------
 function solution(tickets){
    let routes = [];  //가능한 루트들을 담을 배열
 
