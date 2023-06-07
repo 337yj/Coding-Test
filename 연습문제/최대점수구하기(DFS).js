8f588143ef2ba00d56d65bf40ca660da65dbe816
Yun Jeong Lee <110447844+337yj@users.noreply.github.com>
2023-06-07 19:09:39 +0900

    Update 최대점수구하기(DFS).js

--- 연습문제/최대점수구하기(DFS).js
+++ 연습문제/최대점수구하기(DFS).js
@@ -18,3 +18,26 @@ function solution(m, arr) {
 
 const arr = [[10,5],[25,12],[15,8],[6,3],[7,4]];
 console.log(solution(20, arr));
+
+----------------------------------------------------------------
+function solution(m, ps, pt) {
+  let answer = Number.MIN_SAFE_INTEGER;
+
+  function DFS(L, score, time) {
+    if (time > m) return;
+
+    if (L === ps.length) {
+      answer = Math.max(answer, score);
+    } else {
+      DFS(L + 1, score + ps[L], time + pt[L]);
+      DFS(L + 1, score, time);
+    }
+  }
+
+  DFS(0, 0, 0);
+  return answer;
+}
+
+let ps = [10, 25, 15, 6, 7];
+let pt = [5, 12, 8, 3, 4];
+console.log(solution(20, ps, pt));
