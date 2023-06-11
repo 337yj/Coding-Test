41118e86311dd3378860fff7e79bf5cab07b95e6
Yun Jeong Lee <110447844+337yj@users.noreply.github.com>
2023-06-12 03:12:34 +0900

    Update 게임맵최단거리.js

--- 연습문제/게임맵최단거리.js
+++ 연습문제/게임맵최단거리.js
@@ -31,3 +31,33 @@ function solution(board) {
 
   return -1; // 도착점에 도달할 수 없는 경우
 }
+
+--------------------------------------------------------------------------------
+
+function solution(board) {
+  const n = board.length;
+  const m = board[0].length;
+  const dx = [-1, 0, 1, 0];
+  const dy = [0, 1, 0, -1];
+  const queue = [[0, 0, 1]];
+
+  while (queue.length) {
+    const [x, y, cnt] = queue.shift();
+
+    if (x === n - 1 && y === m - 1) {
+      return cnt; // 도착점에 도달한 경우 거리를 반환
+    }
+
+    for (let i = 0; i < 4; i++) {
+      const nx = x + dx[i];
+      const ny = y + dy[i];
+
+      if (nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] === 1) {
+        board[nx][ny] = 0; // 방문한 위치를 0으로 체크
+        queue.push([nx, ny, cnt + 1]);
+      }
+    }
+  }
+
+  return -1; // 도착점에 도달할 수 없는 경우
+}
