399bf237ffa7e4e53751e8b3fdcd7ca4e886a812
Yun Jeong Lee <110447844+337yj@users.noreply.github.com>
2023-06-19 21:04:22 +0900

    Create 숨바꼭질.js

---
+++ 연습문제/숨바꼭질.js
@@ -0,0 +1,33 @@
+const input = require("fs")
+  .readFileSync("예제.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+let n = Number(input[0].split(" ")[0]);
+let k = Number(input[0].split(" ")[1]);
+let ch = new Array(100001).fill(0);
+
+let queue = [];
+queue.push(n);
+ch[n] = 1;
+let cnt = 0;
+
+function BFS() {
+  while (queue.length) {
+    let len = queue.length;
+    for (let i = 0; i < len; i++) {
+      let x = queue.shift();
+      if (x === k) return cnt;
+      for (let nx of [x - 1, x + 1, x * 2]) {
+        if (nx >= 0 && nx <= 100000 && ch[nx] === 0) {
+          ch[nx] = 1;
+          queue.push(nx);
+        }
+      }
+    }
+    cnt++;
+  }
+}
+
+console.log(BFS());
