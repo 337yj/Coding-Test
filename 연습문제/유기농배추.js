f2d6d326a227c0946afb5703d4c65b23987c3ac4
Yun Jeong Lee <110447844+337yj@users.noreply.github.com>
2023-07-24 19:22:31 +0900

    Update 유기농배추.js

--- 연습문제/유기농배추.js
+++ 연습문제/유기농배추.js
@@ -58,4 +58,56 @@ function solution(input) {
   }
 }
 
+
+
+// ---------------------------------------------------------------------
+const input = require("fs")
+  .readFileSync("예제.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+function dfs(graph, n, m, x, y) { // 깊이 우선 탐색
+  // 주어진 범위를 벗어나는 경우 즉시 종료
+  if (x <= -1 || x >= n || y <= -1 || y >= m) return false;
+  // 현재 노드를 아직 처리하지 않았다면
+  if (graph[x][y] === 1) {
+    // 해당 노드 방문 처리
+    graph[x][y] = -1;
+    // 상 하 좌 우 위치들도 모두 재귀적으로 호출
+    dfs(graph, n, m, x - 1, y);
+    dfs(graph, n, m, x, y - 1);
+    dfs(graph, n, m, x + 1, y);
+    dfs(graph, n, m, x, y + 1);
+    return true;
+  }
+  return false;
+}
+
+// 테스트 케이스 수
+let testCases = Number(input[0]);
+let line = 1;
+
+while (testCases--) {
+  // 가로,세로,배추가 심어져 있는 위치의 개수
+  let [m, n, k] = input[line].split(" ").map(Number);
+  let graph = [];
+  for (let i = 0; i < n; i++) {
+    graph[i] = new Array(m);
+  }
+  for (let i = 1; i <= k; i++) {
+    let [y, x] = input[line + i].split(" ").map(Number);
+    graph[x][y] = 1;
+  }
+  let answer = 0; // 연결 요소의 수
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      if (dfs(graph, n, m, i, j)) answer++; // 현재 위치에서 DFS 수행
+    }
+  }
+  line += k + 1; // 다음 테스트 케이스
+  console.log(answer);
+}
+
+
 solution(input);
