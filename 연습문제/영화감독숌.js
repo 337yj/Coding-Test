1315f332ab72a15312f25a29ef5de7b84b0c93d9
Yun Jeong Lee <110447844+337yj@users.noreply.github.com>
2023-08-10 22:05:49 +0900

    Create 영화감독숌.js

---
+++ 연습문제/영화감독숌.js
@@ -0,0 +1,17 @@
+let input= require('fs').readFileSync('/dev/stdin').toString();
+
+let title=666;
+
+while(true){
+    
+    if(title.toString().includes('666')){
+        input-=1;
+        if(input==0)
+            break;
+    }
+    
+    title+=1;
+    
+}
+
+console.log(title);
